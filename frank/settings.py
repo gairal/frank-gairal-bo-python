76944aae4d48b2e00dd8c835c14cd63b11581ea7
frank.gairal <frank.gairal@fullsix.com>
2014-11-20 21:13:01 +0700

    Fix heroku settings error

--- frank/settings.py
+++ frank/settings.py
@@ -11,15 +11,19 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-try:
-    from local_settings import *
-except ImportError:
-    pass
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', True)
+TEMPLATE_DEBUG = os.getenv('TEMPLATE_DEBUG', True)
+SECRET_KEY = os.getenv('SECRET_KEY', 'toto1234')
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://frankgairal@localhost:5432/frank')}
 
 # Application definition
 
@@ -64,7 +68,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
